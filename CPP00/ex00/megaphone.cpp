2b25f0de630b13dd9a9f4d580bc81e8cc2ac75e8
Kiono78 <40069205+Kiono78@users.noreply.github.com>
2022-06-14 16:15:21 +0200

    CPP00/ex00

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bterral <bterral@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/14 13:45:06 by bterral           #+#    #+#             */
+/*   Updated: 2022/06/14 14:26:44 by bterral          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cstring>
+
+int main(int argc, char **argv)
+{
+	if (argc < 2)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else if (argv)
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			for (size_t j = 0; j < strlen(argv[i]); j++)
+			{
+				putchar(toupper(argv[i][j]));
+			}
+		}
+	}
+	std::cout << std::endl;
+	return (0);
+}
