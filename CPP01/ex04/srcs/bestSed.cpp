0e3e01fc319ecb9bd87edea1dee3b2814c46385f
Kiono78 <benoit.terral@gmail.com>
2022-08-06 16:56:40 +0200

    spaghetti code ex04

--- CPP01/ex04/srcs/bestSed.cpp
+++ CPP01/ex04/srcs/bestSed.cpp
@@ -6,7 +6,7 @@
 /*   By: bterral <bterral@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/29 14:54:08 by bterral           #+#    #+#             */
-/*   Updated: 2022/08/06 08:55:31 by bterral          ###   ########.fr       */
+/*   Updated: 2022/08/06 16:40:29 by bterral          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,10 @@ int main( int argc, char **argv )
 	std::vector<std::string>				words;
 	int										nbWords = 0;
 	std::vector<std::string> 				stringArgs(argv, argv + argc);
-	std::ifstream							ifs(argv[1]);
 	std::string								newFileName;
-	std::string								fileName(argv[1]);
+	std::ifstream							ifs;
 	std::ofstream							ofs;
+	std::string								line;
 
 
 	if (argc != 4)
@@ -29,17 +29,28 @@ int main( int argc, char **argv )
 		std::cout << RED << "3 arguments expected: FILE_NAME SEARCH_STRING REPLACEMENT" << RESET << std::endl;
 		return (1);
 	}
+	//ifs.unsetf(std::ios::skipws);
+	ifs.open(argv[1]);
 	if (!ifs)
 	{
 		std::cout << RED << "Failed to open file" << RESET << std::endl;
 		return (1);
 	}
-	while ( ifs >> word )
+	std::string								fileName(argv[1]);
+	while (getline(ifs, line))
 	{
-		if (word == stringArgs[2])
-			words.push_back( stringArgs[3] );
-		else
-			words.push_back ( word );
+		std::istringstream buffer(line);
+		while ( buffer >> word )
+		{
+			if (word == stringArgs[2])
+				words.push_back( stringArgs[3] );
+			else
+				words.push_back ( word );
+			words.push_back (" ");
+			nbWords = nbWords + 2;
+		}
+		if (!ifs.eof())
+			words.push_back("\n");
 		nbWords++;
 	}
 	newFileName = fileName + ".replace";
